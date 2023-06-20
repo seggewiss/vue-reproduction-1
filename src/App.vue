546438bbe3db11af8b88d25e638f85e8f07d6e9b
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-06-20 13:50:18 +0200

    Init reproduction repo

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Problem from './components/Problem.vue'
+</script>
+
+<template>
+  <main>
+    <Problem />
+  </main>
+</template>
---
+++ src/components/Problem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import Criteria from 'node_modules/@shopware-ag/admin-extension-sdk/es/data/Criteria';
+
+defineComponent({
+  name: 'TheWelcome',
+
+  template: '<h1>Welcome to Vue 3</h1>',
+
+  data(): { var: Criteria|null } {
+    return {
+      var: null,
+    };
+  },
+
+  created() {
+    this.var = new Criteria();
+
+    this.sayHello(this.var); // The type does not match the expected type 'Criteria'.
+  },
+
+  methods: {
+    sayHello(v: Criteria) {
+      console.log('Hello', v);
+    },
+  },
+});
+</script>
